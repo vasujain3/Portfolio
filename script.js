b56242878fce9e4582f0c786413c764c0a938b56
Vasu <vasu.jain333@gmail.com>
2022-07-14 20:13:11 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function() {
+    $("html , body").animate({ scrollTop: 0 }, "slow");
+});
